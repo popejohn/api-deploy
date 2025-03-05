de6620994aeb3cf3815051f239365b16309e1cc4
popejohn <popoolaj63@yahoo.com>
2025-03-05 13:53:09 +0100

    updated code

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const app = express()
 app.get("/", (req, res) => {
     // res.send('Welcome home')
     res.json([
-        {"name": "Bimbo", "class": "React"},
-        {"name": "Cletus", "class": "Java"},
-        {"name": "Clove", "class": "React"},
-        {"name": "Frodo", "class": "Node"}
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Play game", "content": "Play a chess game", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Code", "content": "Install VS Code", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Watch movie", "content": "Watch movie on netflix", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Practice", "content": "Practice makes perfect ", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Test", "content": "Pinty piety pay", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Classes", "content": "Play a chess game", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Show movie", "content": "djhge", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Take course", "content": "Play a chess game", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Application", "content": "Play a chess game", "Completed": "true"},
+        {"todo_id":Math.floor(Math.random() * 100 + 1), "Title": "Show interest", "content": "Play a chess game", "Completed": "true"},
     ])
 })
 
